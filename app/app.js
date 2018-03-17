1493b634f7f9e60abdb6a30d60003f749d5b8fcd
Amakawa <firesidecake@gmail.com>
2018-03-18 02:59:25 +1100

    Update package.json modules to latest version

--- app/app.js
+++ app/app.js
@@ -5,8 +5,50 @@ let App = React.createClass({
   render: () => {
     return (
       <div>
+        <div id="header"></div>
+        <div className="container">
+          <div className="column">
+            <InboxPane />
+          </div>
+          <div className="column">
 
+          </div>
+        </div>
       </div>
+    )
+  }
+});
+
+let InboxPane = React.createClass({
+  render: () => {
+    return (
+      <div id="inbox-pane">
+        <h1>Inbox</h1>
+        <table>
+          <thread>
+            <tr>
+              <th>Chat Received</th>
+              <th>Name</th>
+              <th>Status</th>
+            </tr>
+            <tbody>
+              <InboxItem />
+            </tbody>
+          </thread>
+        </table>
+      </div>
+    );
+  }
+});
+
+let InboxItem = React.createClass({
+  render: () => {
+    return (
+      <tr>
+        <td>5pm</td>
+        <td>Rami loves pizza</td>
+        <td>Confirmed</td>
+      </tr>
     )
   }
 })
